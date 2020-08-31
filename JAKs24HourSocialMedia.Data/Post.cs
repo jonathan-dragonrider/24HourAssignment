c1caac9c475c055b8d0aaf80e74ad53aa2e0506e
Jon Kline <jonphilk@gmail.com>
2020-08-31 15:06:53 -0400

    added Post, CreatePost

--- JAKs24HourSocialMedia.Data/Post.cs
+++ JAKs24HourSocialMedia.Data/Post.cs
@@ -1,12 +1,25 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace JAKs24HourSocialMedia.Data
 {
-    class Post
+    public class Post
     {
+        [Key]
+        public int PostId { get; set; }
+
+        [Required]
+        public string Title { get; set; }
+
+        [Required]
+        public string Text { get; set; }
+
+        [ForeignKey("User")]
+        public int UserId { get; set; } 
     }
 }

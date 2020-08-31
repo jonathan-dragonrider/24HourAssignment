197f40afcb70c4973fa29cf234822cdb45e5b044
Kyle Rambo <krambo89@gmail.com>
2020-08-31 17:00:51 -0400

    controller update

--- JAKs24HourSocialMedia.WebAPI/Controllers/UserController.cs
+++ JAKs24HourSocialMedia.WebAPI/Controllers/UserController.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using JAKs24HourSocialMedia.Models;
+using JAKs24HourSocialMedia.Services;
+using Microsoft.AspNet.Identity;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
@@ -7,7 +10,33 @@ using System.Web.Http;
 
 namespace JAKs24HourSocialMedia.WebAPI.Controllers
 {
+    [Authorize]
     public class UserController : ApiController
     {
+        private UserService CreateUserService()
+        {
+            var userId = Guid.Parse(User.Identity.GetUserId());
+            var userService = new UserService(userId);
+            return userService;
+        }
+
+        public IHttpActionResult Get()
+        {
+            UserService userService = CreateUserService();
+            var users = userService.GetUsers();
+            return Ok(users);
+        }
+        public IHttpActionResult Post(CreateUser user)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            var service = CreateUserService();
+
+            if (!service.UserCreate(user))
+                return InternalServerError();
+
+            return Ok();
+        }
     }
 }

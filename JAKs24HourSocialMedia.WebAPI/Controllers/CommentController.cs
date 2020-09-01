aac7d0eab938e749f73d751e673f68c5b9a27b6f
Alexander.Blansette@gmail.com <Alexander.Blansette@gmail.com>
2020-09-01 11:01:26 -0400

    ListController

--- JAKs24HourSocialMedia.WebAPI/Controllers/CommentController.cs
+++ JAKs24HourSocialMedia.WebAPI/Controllers/CommentController.cs
@@ -3,6 +3,7 @@ using JAKs24HourSocialMedia.Models;
 using JAKs24HourSocialMedia.Services;
 using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
@@ -16,7 +17,7 @@ namespace JAKs24HourSocialMedia.WebAPI.Controllers
     {
         //CRUD (PGPD)
         //Post
-        public IHttpActionResult Post(CommentCreate comment)
+        /*public IHttpActionResult Post(CommentCreate comment)
         {
             if (!ModelState.IsValid)
                 return BadRequest(ModelState);
@@ -43,19 +44,14 @@ namespace JAKs24HourSocialMedia.WebAPI.Controllers
             return Ok(comment);
         }
 
-        public IHttpActionResult GetById(int id)
-        {
-            Comment comment = Comment.Find(id);
-        }
-
-        public NoteDetail GetNoteById(int id)
+        public IHttpActionResult GetNoteById(int id)
         {
             using (var ctx = new ApplicationDbContext())
             {
                 var entity =
                     ctx
                         .Comments
-                        .Single(e => e.Id == id && e.userId == _userId);
+                        .Single(e => e.Id == id && e.Id == _userId);
                 return
                     new CommentDetail
                     {
@@ -65,6 +61,84 @@ namespace JAKs24HourSocialMedia.WebAPI.Controllers
                         CommentPost = entity.CommentPost
                     };
             }
+        }*/
+        private readonly ApplicationDbContext _context = new ApplicationDbContext();
+        // CRUD (PGPD)
+
+        // (Post) Create
+        [HttpPost]
+        public async Task<IHttpActionResult> PostComment(Comment model)
+        {
+            if (model == null)
+            {
+                return BadRequest("Your request cannot be empty");
+            }
+            if (ModelState.IsValid)
+            {
+                _context.Comments.Add(model);
+                await _context.SaveChangesAsync();
+                return Ok("You've created a new Customer");
+            }
+            return BadRequest(ModelState);
+        }
+
+        // (GetbyId) Read
+        [HttpGet]
+        public async Task<IHttpActionResult> GetById(int id)
+        {
+            Comment comment = await _context.Comments.FindAsync(id);
+            if (comment != null)
+            {
+                return Ok(comment);
+            }
+            return NotFound();
+        }
+
+        // (GetAll) Read
+        [HttpGet]
+        public async Task<IHttpActionResult> GeAll()
+        {
+            List<Comment> comment = await _context.Comments.ToListAsync();
+            return Ok(comment);
+        }
+
+        // (Put) Update
+        [HttpPut]
+        public async Task<IHttpActionResult> UpdateComment([FromUri] int id, [FromBody] Comment updatedComment)
+        {
+            if (ModelState.IsValid)
+            {
+                Comment comment = await _context.Comments.FindAsync(id);
+                if (comment != null)
+                {
+                    comment.Text = updatedComment.Text;
+
+                    await _context.SaveChangesAsync();
+                    return Ok();
+                }
+                return NotFound();
+            }
+            return BadRequest(ModelState);
+        }
+
+        // (Delete) Delete
+        [HttpDelete]
+        public async Task<IHttpActionResult> DeleteComment(int id)
+        {
+            Comment entity = await _context.Comments.FindAsync(id);
+            if (entity == null)
+            {
+                return NotFound();
+            }
+
+            _context.Comments.Remove(entity);
+
+            if (await _context.SaveChangesAsync() == 1)
+            {
+                return Ok("Comment has been deleted");
+            }
+
+            return InternalServerError();
         }
     }
 }
